d193f7e75a244b0da4f16d7b9890d0c97ae92046
SmotritelTerve <peacemo@protonmail.ch>
2020-02-10 19:49:36 +0300

    started learn design patterns

---
+++ js_patterns/01_creational/02_factory.js
@@ -0,0 +1,22 @@
+// допустим у нас есть коллекция элементов со схожим функционалом,
+// например, разные виды подписки
+class SimpleMembership{
+    constructor(name){
+        this.name = name;
+        this.cost = 50;
+    }
+}
+
+class StandardMembership{
+    constructor(name){
+        this.name = name;
+        this.cost = 150;
+    }
+}
+
+class PremiumMembership{
+    constructor(name){
+        this.name = name;
+        this.cost = 50;
+    }
+}
